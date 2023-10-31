5b760ba6463aaae1fa198939094cf4a805b9f731
JeongHwan Kim <rocky990903@naver.com>
2023-10-31 19:17:22 +0900

    과연
    되려나

--- src/main/java/com/sparta/team2project/logging/LoggingController.java
+++ src/main/java/com/sparta/team2project/logging/LoggingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoggingController {
+
     @Value("${logging-module.version}")
     private String version;
 
