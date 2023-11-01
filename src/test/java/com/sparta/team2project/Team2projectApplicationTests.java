1f6545172bfd5534dc5dfab329fbe71cdc2138a9
JeongHwan Kim <rocky990903@naver.com>
2023-11-02 01:53:07 +0900

    변경
    완료

--- src/test/java/com/sparta/team2project/Team2projectApplicationTests.java
+++ src/test/java/com/sparta/team2project/Team2projectApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.team2project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Team2projectApplicationTests {
 
 	@Test
