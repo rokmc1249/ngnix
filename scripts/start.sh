fe9a168e3d320556d1088b5dc4aebc47aeec6652
JeongHwan Kim <rocky990903@naver.com>
2023-11-04 03:33:54 +0900

    집가자
    가자

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,13 +6,13 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ubuntu/app/step3
+REPOSITORY=/home/ubuntu/app
 PROJECT_NAME=Galle
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/deloy/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
