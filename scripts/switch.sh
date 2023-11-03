8d832fc38dc677d4f01df38f997e37cb4cc150d0
JeongHwan Kim <rocky990903@naver.com>
2023-11-04 00:01:39 +0900

    되라
    제발

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
-CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
-TARGET_PORT=0
 
-echo "Nginx currently proxies to ${CURRENT_PORT}."
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-if [ ${CURRENT_PORT} -eq 8089 ]; then
-  TARGET_PORT=8090
-elif [ ${CURRENT_PORT} -eq 8090 ]; then
-  TARGET_PORT=8089
-else
-  echo "No WAS is connected to nginx"
-  exit 1
-fi
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_port)
 
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 전환"
+    # 하나의 문장을 만들어 파이프라인(|)으로 넘겨 주기 위해 echo 사용
+    # 엔진엑스가 변경할 프록시 주소 생성
+    # 쌍따옴표를 사용하지 않으면 $service_url를 인식하지 못하고 변수를 찾는다
+    # 앞에서 넘겨 준 문장을 service-url.inc에 담는다
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-echo "Now Nginx proxies to ${TARGET_PORT}."
-sudo service nginx reload
-
-echo "Nginx reloaded."
+    echo "> 엔진엑스 Reload"
+    sudo service nginx reload # 엔진엑스 설정을 다시 불러온다 (외부 설정 파일인 service-url)
+}
--- src/main/java/com/sparta/team2project/pro/HealthController.java
+++ src/main/java/com/sparta/team2project/pro/HealthController.java
@@ -1,15 +1,30 @@
 package com.sparta.team2project.pro;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 @RestController
+@RequiredArgsConstructor
 public class HealthController {
 
-    @GetMapping("/health")
-    public String checkHealth() {
-        return "healthy1";
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
 
