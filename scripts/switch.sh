f55f24810a8152b00efad4ccb1eab80a2de71bc7
JeongHwan Kim <rocky990903@naver.com>
2023-11-04 20:50:11 +0900

    재시도
    재시도

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -13,7 +13,7 @@ function switch_proxy() {
     # 엔진엑스가 변경할 프록시 주소 생성
     # 쌍따옴표를 사용하지 않으면 $service_url를 인식하지 못하고 변수를 찾는다
     # 앞에서 넘겨 준 문장을 service-url.inc에 담는다
-    echo "set \$service_url http://43.202.202.19:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set \$service_url http://3.37.107.30:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
     sudo service nginx reload # 엔진엑스 설정을 다시 불러온다 (외부 설정 파일인 service-url)
