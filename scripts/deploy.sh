9858a62bb5c5f457c71f3cfed99c2284eaf5329c
JeongHwan Kim <rocky990903@naver.com>
2023-11-07 21:53:05 +0900

    deploy scripts 변경
    입니다

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,26 +4,28 @@ cd /home/ubuntu/app
 
 DOCKER_APP_NAME=spring
 
-# 실행중인 blue가 있는지
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+# Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yaml ps | grep Up)
 
-# green이 실행중이면 blue up
+# 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-
-	sleep 30
-
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-	docker image prune -af # 사용하지 않는 이미지 삭제
-
-# blue가 실행중이면 green up
+    echo "blue up"
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yaml up -d
+    BEFORE_COMPOSE_COLOR="green"
+    AFTER_COMPOSE_COLOR="blue"
 else
-	echo "green up"
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+    echo "green up"
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yaml up -d
+    BEFORE_COMPOSE_COLOR="blue"
+    AFTER_COMPOSE_COLOR="green"
+fi
 
-	sleep 30
+sleep 10
 
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-	docker image prune -af
+# 새로운 컨테이너가 제대로 떴는지 확인
+EXIST_AFTER=$(docker-compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
+if [ -n "$EXIST_AFTER" ]; then
+    # 이전 컨테이너 종료
+    docker-compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
+    echo "$BEFORE_COMPOSE_COLOR down"
 fi
