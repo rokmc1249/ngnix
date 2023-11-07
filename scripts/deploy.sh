eb5033a7a4a004b0aa46a37651215798c5bdb649
JeongHwan Kim <rocky990903@naver.com>
2023-11-07 22:00:10 +0900

    오타 정정
    오타 정정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,17 +5,17 @@ cd /home/ubuntu/app
 DOCKER_APP_NAME=spring
 
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yaml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yaml up -d
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
 else
     echo "green up"
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yaml up -d
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
 fi
@@ -23,9 +23,9 @@ fi
 sleep 10
 
 # 새로운 컨테이너가 제대로 떴는지 확인
-EXIST_AFTER=$(docker-compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
+EXIST_AFTER=$(docker-compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
     # 이전 컨테이너 종료
-    docker-compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
+    docker-compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
     echo "$BEFORE_COMPOSE_COLOR down"
 fi
